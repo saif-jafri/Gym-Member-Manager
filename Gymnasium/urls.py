9d64501405a7263e04ca77285ef1835d3c3103e4
Mohd Saif <saif_jafri786@rediffmail.com>
2021-06-03 01:05:06 +0530

    deploy heroku

--- Gymnasium/urls.py
+++ Gymnasium/urls.py
@@ -18,8 +18,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.views.static import serve
-from django.conf.urls import url
+#from django.views.static import serve
+#from django.conf.urls import url
 
 
 urlpatterns = [
@@ -30,8 +30,8 @@ urlpatterns = [
     path('reports/', include('reports.urls')),
     path('', include('accounts.urls'))
     
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+  #  url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+  # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 ]
 
