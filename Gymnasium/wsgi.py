f2202bd8ad22cbd5b92323b132658faa0aafa3fb
Mohd Saif <saif_jafri786@rediffmail.com>
2021-06-03 14:43:08 +0530

    Update wsgi
    #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Gymnasium.settings")
    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Gymnasium.settings.dev")

--- Gymnasium/wsgi.py
+++ Gymnasium/wsgi.py
@@ -11,6 +11,7 @@ import os
 from notifications.config import get_notification_count, run_notifier
 from django.core.wsgi import get_wsgi_application
 run_notifier()
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Gymnasium.settings")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Gymnasium.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Gymnasium.settings.dev")
 
 application = get_wsgi_application()
